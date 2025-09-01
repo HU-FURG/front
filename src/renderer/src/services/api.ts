a3da7310b4368945fb862f37f75cd64386513ff4
Luiz Gustavo S Lopes <97141086+lopesgustavo01@users.noreply.github.com>
2025-09-01 15:12:44 -0300

    Update api.ts

--- src/renderer/src/services/api.ts
+++ src/renderer/src/services/api.ts
@@ -2,21 +2,21 @@
 // services/api.ts
 import axios from 'axios'
 
-export const api = axios.create({
-  baseURL: 'http://localhost:3333/api', 
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
-
 // export const api = axios.create({
-//   baseURL: 'https://precious-reyna-hu-furg-b9ddc9e2.koyeb.app/api',
+//   baseURL: 'http://localhost:3333/api', 
 //   withCredentials: true,
 //   headers: {
 //     'Content-Type': 'application/json',
 //   },
 // })
+
+export const api = axios.create({
+  baseURL: 'https://precious-reyna-hu-furg-b9ddc9e2.koyeb.app/api',
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
 
 // Interceptor de resposta: se JWT inválido, desloga
 api.interceptors.response.use(
